743bf9f668dcdbb6812ea46d44cb1a5f46699eec
shubham <vickygaikwad41996@gmail.com>
2025-04-08 12:26:19 +0530

    update bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "nike-main-bucket"
+    bucket         = "nike-terraform-tfstate-file-bucket"
     key            = "vpcJenkinsServertfState/state.tfstate"
     region         = "us-east-1"
     encrypt        = true
